306307b9e746135e820de851369a129df3f63216
ramon.rdm <ramon.rdm@gmail.com>
2014-09-30 18:21:05 -0300

    add requisitos

--- agenda/views.py
+++ agenda/views.py
@@ -9,3 +9,7 @@ def index(request):
 	titulo = "Agendador de espaço físico do CCS"
 	corpo = "Bem vindo ao Agendador de espaço físico do CCS"
 	return render_to_response("index.html",{'corpo':corpo,"titulo":titulo})
+
+def requisitos(request):
+	titulo = "Requisitos do Agendador CCS"
+	return render_to_response("requisitos.html", {'titulo':titulo})
--- agendador/urls.py
+++ agendador/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'agenda.views.index', name='index'),
+    url(r'^requisitos', 'agenda.views.requisitos', name='requisitos'),
 )
