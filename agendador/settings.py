3768e7abada91609e6da4641373354252191a079
Thiago Brezinski <thiagobrez@gmail.com>
2017-05-16 17:13:34 -0300

    too late

--- agendador/settings.py
+++ agendador/settings.py
@@ -74,12 +74,8 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'agendador',
-        'USER': 'agendador',
-        'PASSWORD': 'agendador',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
