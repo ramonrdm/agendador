70d6672710adb0ad76230ce6cd7e3ba0f5b5932e
ramon.rdm <ramon.rdm@gmail.com>
2017-06-13 10:49:11 -0300

    limitando agendamentos espaço 3 somente a 2017 até 02/10

--- agenda/views.py
+++ agenda/views.py
@@ -114,8 +114,8 @@ def addreserva(request, espacoatual, ano=None, mes= None, dia=None):
             
             if ((not request.user.groups.filter(name="LABINFO").exists()) and (request.POST['espacoFisico']=='3')):
                 return render_to_response("salvo.html",{'mensagem':"Erro: Você não realizaou o curso para utilizar o labinfo "})
-            if (request.POST['espacoFisico']=='3' and (date.today() < date(2017,10,02))):
-                return render_to_response("salvo.html",{'mensagem':"Erro: Espaço bloqueado para reservas até 02/10/2017 "})
+            if (request.POST['espacoFisico']=='3' and (date.today() < date(2017,10,02) and form.fields['date'] > date(2017,12,31) )):
+                return render_to_response("salvo.html",{'mensagem':"Erro: Espaço bloqueado para reservas em 2018 até 02/10/2017 "})
             
             if ((not request.user.groups.filter(name="CONSELHEIROS").exists()) and (request.POST['espacoFisico']=='4')):
                 return render_to_response("salvo.html",{'mensagem':"Erro: Você não é membro do grupo sala dos conselhos, você não pode reservar este espaço."})
