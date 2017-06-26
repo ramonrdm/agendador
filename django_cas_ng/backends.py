928f3a9a033aac5f24438fede435d8597e684ae5
ramonrdm <ramon.rdm@gmail.com>
2017-06-26 18:37:44 -0300

    correção de acentuação e caracteres especiais

--- django_cas_ng/backends.py
+++ django_cas_ng/backends.py
@@ -252,8 +252,8 @@ def _verify_cas3_saml(ticket, service):
     try:
         user = None
         attributes = {}
-        response = page.read().replace('\n','')
-        response = response.encode("utf-8")
+        response = page.read()#.replace('\n','')
+        #response = response.encode("utf-8")
         tree = ElementTree.fromstring(response)
         #print response
         # Find the authentication status
