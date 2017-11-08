0b8c02752f4cde56a785a659d3f63dfe035a57a0
ramonrdm <ramon.rdm@gmail.com>
2017-11-08 16:27:31 -0200

    ajustando wsgi para docker

--- agendador/wsgi.py
+++ agendador/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for agendador project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
 import sys
-sys.path.append('/home/reservasccs/public_html/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agendador.settings")
-
 from django.core.wsgi import get_wsgi_application
+
+#sys.path.append('/home/reservasccs/public_html/')
+sys.path.append('/var/www/html/agendador')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agendador.settings")
 application = get_wsgi_application()
