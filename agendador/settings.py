fc4a87060e0a9536e4a4882a7e453652a6aedb7b
ramonrdm <ramon.rdm@gmail.com>
2017-11-10 18:33:52 -0200

    bd test

--- agendador/settings.py
+++ agendador/settings.py
@@ -70,8 +70,12 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'submitlibras',
+        'USER': 'submitlibras',
+        'PASSWORD': '',
+        'HOST': 'mysql.sites.ufsc.br',
+        'PORT': '3306',
     }
 }
 
