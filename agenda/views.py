cd7258fe13b920283f22d0f6895f136736d57cc6
gabrieltron <joao_trombeta@hotmail.com>
2017-12-04 16:56:18 -0200

    m

--- agenda/views.py
+++ agenda/views.py
@@ -13,7 +13,6 @@ from agenda.models import *
 from agenda.forms import ReservaEspacoFisicoAdminForm
 from django import forms
 from django.contrib.admin.sites import AdminSite
-from dal import autocomplete
 from datetime import date
 from forms import *
 
@@ -232,4 +231,3 @@ def intermediaria(request, tipo, id_equip, data_numero):
         return redirect('/admin/agenda/reservaequipamento/add/')
     elif tipo == 'f':
         return redirect('/admin/agenda/reservaespacofisico/add/')
-
